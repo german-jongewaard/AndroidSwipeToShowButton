71f94e35ba1d252a154cccd5ae2de03a3d99ab88
german-jongewaard <germanjongewaard@gmail.com>
2019-09-12 14:12:06 +0200

    Swipe to show

---
+++ app/src/main/java/com/jongewaard/dev/androidswipetoshowbutton/Helper/MyButtonClickListener.java
@@ -0,0 +1,5 @@
+package com.jongewaard.dev.androidswipetoshowbutton.Helper;
+
+interface MyButtonClickListener {
+    void onClick(int pos);
+}
--- app/src/main/java/com/jongewaard/dev/androidswipetoshowbutton/Helper/MySwipeHelper.java
+++ app/src/main/java/com/jongewaard/dev/androidswipetoshowbutton/Helper/MySwipeHelper.java
@@ -1,4 +1,129 @@
 package com.jongewaard.dev.androidswipetoshowbutton.Helper;
 
-public class MySwipeHelper {
-}
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class MySwipeHelper extends ItemTouchHelper.SimpleCallback {
+
+    int buttonWidth;
+    private RecyclerView recyclerView;
+    private List<MyButton> buttonList;
+    private GestureDetector gestureDetector;
+    private int swipePosition = -1;
+    private float swipeThreshold = 0.5f;
+    private Map<Integer, List<MyButton>> buttonBuffer;
+    private Queue<Integer> removeQueue;
+
+    private GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener(){
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+
+            for(MyButton button:buttonList){
+
+            }
+            return super.onSingleTapUp(e);
+        }
+    };
+
+
+
+
+
+    public MySwipeHelper(int dragDirs, int swipeDirs) {
+        super(dragDirs, swipeDirs);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
+
+    private class MyButton {
+        private String text;
+        private int imageResId, textSize, color, pos;
+        private RectF clickRegion;
+        private MyButtonClickListener listener;
+        private Context context;
+        private Resources resources;
+
+
+        public MyButton(Context context, String text, int imageResId, int textSize, int color, int pos, RectF clickRegion, MyButtonClickListener listener) {
+            this.text = text;
+            this.imageResId = imageResId;
+            this.textSize = textSize;
+            this.color = color;
+            this.pos = pos;
+            this.clickRegion = clickRegion;
+            this.listener = listener;
+            this.context = context;
+            resources = context.getResources();
+        }
+
+        public boolean onclick(float x, float y){
+
+            if(clickRegion != null && clickRegion.contains(x,y)){
+
+                listener.onClick(pos);
+                return true;
+            }
+            return false;
+        }
+
+        public void onDraw(Canvas c, RectF rectF, int pos)
+        {
+            Paint p = new Paint();
+            p.setColor(color);
+            c.drawRect(rectF, p);
+            //text
+            p.setColor(Color.WHITE);
+            p.setTextSize(textSize);
+
+            Rect r = new Rect();
+            float cHeight = rectF.height();
+            float cWidth = rectF.width();
+            p.setTextAlign(Paint.Align.LEFT);
+            p.getTextBounds(text, 0, text.length(), r);
+            float x=0, y=0;
+            if(imageResId == 0) //If just show text
+            {
+                x = cWidth/2f - r.width()/2f - r.left;
+                y = cHeight/2f + r.height()/2f - r.bottom;
+                c.drawText(text, rectF.left+x, rectF.top+y, p);
+            } else{
+
+                Drawable d = ContextCompat.getDrawable(context, imageResId);
+                Bitmap bitmap = drawableToBitmap(d);
+                c.drawBitmap(bitmap, (rectF.left + rectF.right)/2, (rectF.top+rectF.bottom)/2, p);
+
+            }
+            clickRegion = rectF;
+            this.pos = pos;
+        }
+    }
+
+    private Bitmap drawableToBitmap(Drawable d) {
+
+    }
